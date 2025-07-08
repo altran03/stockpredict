317ddc8ce99162672d0479a6e8b541c8f4fd0e55
alvintran <tralvin3@gmail.com>
2025-07-08 12:56:18 -0700

    fetch api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/api/hello')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error:', error));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Stock Predictor</h1>
+      <p>Backend says: {message}</p>
     </div>
   );
 }
